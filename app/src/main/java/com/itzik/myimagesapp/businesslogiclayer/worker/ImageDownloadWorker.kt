05d5ee07dc8d6de7082d6f1b927dacf87d1be041
itzik <itzikrb100@gmail.com>
2022-11-05 22:49:19 +0200

    update insert to db

--- app/src/main/java/com/itzik/myimagesapp/businesslogiclayer/worker/ImageDownloadWorker.kt
+++ app/src/main/java/com/itzik/myimagesapp/businesslogiclayer/worker/ImageDownloadWorker.kt
@@ -6,9 +6,12 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.itzik.myimagesapp.Utils.Consts
 import com.itzik.myimagesapp.Utils.Consts.NUMBER_IMAGES
+import com.itzik.myimagesapp.Utils.TimeUtil
 import com.itzik.myimagesapp.api.ApiClientService
 import com.itzik.myimagesapp.api.ImageApi
 import com.itzik.myimagesapp.api.model.ImageResponse
+import com.itzik.myimagesapp.repo.ImagesRepo
+import com.itzik.myimagesapp.repo.entities.ImageEntity
 
 
 class ImageDownloadWorker(context: Context, parameters: WorkerParameters):
@@ -18,17 +21,24 @@ class ImageDownloadWorker(context: Context, parameters: WorkerParameters):
 
     override suspend fun doWork(): Result {
         // call from  Coroutine
-
        val result =  api.doGetListImages(NUMBER_IMAGES)
        if(result.isSuccessful) {
            val imagesResponse: ImageResponse? = result.body()
-
            if(!imagesResponse?.message.isNullOrEmpty()) {
                // insert to db
-
+               insertImages(imagesResponse!!.message)
            }
        }
-        Log.d("itzik- ImageDownloadWorker", "do work download image thread = ${Thread.currentThread().name}")
-        return Result.success()
+       Log.d("itzik- ImageDownloadWorker", "do work download image thread = ${Thread.currentThread().name}")
+       return Result.success()
+    }
+
+
+    private suspend fun insertImages(urls: List<String>) {
+        val images = emptyList<ImageEntity>().toMutableList()
+        for(url in urls) {
+            images.add(ImageEntity(TimeUtil.getTime(), TimeUtil.getCurrentDate(), url))
+        }
+        ImagesRepo.addImages(images)
     }
 }
